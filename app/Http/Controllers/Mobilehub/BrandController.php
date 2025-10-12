d4bc44f2c52f6f71d53d55714ad1a86649aea488
tajmilur0 <tajmilur.github@gmail.com>
2025-10-12 23:24:08 +0600

    Refactor README.md file and code structure for improved readability and maintainability

--- app/Http/Controllers/Mobilehub/BrandController.php
+++ app/Http/Controllers/Mobilehub/BrandController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use Illuminate\Http\Request;
 use Intervention\Image\Laravel\Facades\Image;
+use Intervention\Image\ImageManager;
+  use Intervention\Image\Drivers\Gd\Driver;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
@@ -39,55 +41,123 @@ class BrandController extends Controller
         $brand = new Brand();
         $brand->name = $request->name;
 
-        // Icon
-        if ($request->hasFile('brand_icon')) {
-            $brandName = Str::slug($request->name);
-            $uniqueId = uniqid();
-            $iconName = "{$brandName}_icon_{$uniqueId}." . $request->file('brand_icon')->getClientOriginalExtension();
+        // // Icon
+        // if ($request->hasFile('brand_icon')) {
+        //     $brandName = Str::slug($request->name);
+        //     $uniqueId = uniqid();
+        //     $iconName = "{$brandName}_icon_{$uniqueId}." . $request->file('brand_icon')->getClientOriginalExtension();
 
-            $icon = Image::make($request->file('brand_icon'));
-            $icon->resize(200, 200, function ($constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-            });
+        //     $icon = Image::make($request->file('brand_icon'));
+        //     $icon->resize(200, 200, function ($constraint) {
+        //         $constraint->aspectRatio();
+        //         $constraint->upsize();
+        //     });
 
-            $iconPath = 'uploads/brands/icons/';
+        //     $iconPath = 'uploads/brands/icons/';
 
-            // Create folder if not exists
-            if (!File::exists(public_path($iconPath))) {
-                File::makeDirectory(public_path($iconPath), 0755, true);
-            }
+        //     // Create folder if not exists
+        //     if (!File::exists(public_path($iconPath))) {
+        //         File::makeDirectory(public_path($iconPath), 0755, true);
+        //     }
 
-            $icon->save(public_path($iconPath . $iconName));
-            $brand->brand_icon = $iconPath . $iconName;
-        } else {
-            $brand->brand_icon = null;
-        }
+        //     $icon->save(public_path($iconPath . $iconName));
+        //     $brand->brand_icon = $iconPath . $iconName;
+        // } else {
+        //     $brand->brand_icon = null;
+        // }
 
-        // Main Image
-        if ($request->hasFile('brand_image')) {
-            $brandName = Str::slug($request->name);
-            $uniqueId = uniqid();
-            $imageName = "{$brandName}_image_{$uniqueId}." . $request->file('brand_image')->getClientOriginalExtension();
+        // // Main Image
+        // if ($request->hasFile('brand_image')) {
+        //     $brandName = Str::slug($request->name);
+        //     $uniqueId = uniqid();
+        //     $imageName = "{$brandName}_image_{$uniqueId}." . $request->file('brand_image')->getClientOriginalExtension();
 
-            $image = Image::make($request->file('brand_image'));
-            $image->resize(800, 800, function ($constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-            });
+        //     $image = Image::make($request->file('brand_image'));
+        //     $image->resize(800, 800, function ($constraint) {
+        //         $constraint->aspectRatio();
+        //         $constraint->upsize();
+        //     });
 
-            $imagePath = 'uploads/brands/images/';
+        //     $imagePath = 'uploads/brands/images/';
 
-            if (!File::exists(public_path($imagePath))) {
-                File::makeDirectory(public_path($imagePath), 0755, true);
-            }
+        //     if (!File::exists(public_path($imagePath))) {
+        //         File::makeDirectory(public_path($imagePath), 0755, true);
+        //     }
 
-            $image->save(public_path($imagePath . $imageName));
-            $brand->brand_image = $imagePath . $imageName;
-        } else {
-            $brand->brand_image = null;
-        }
+        //     $image->save(public_path($imagePath . $imageName));
+        //     $brand->brand_image = $imagePath . $imageName;
+        // } else {
+        //     $brand->brand_image = null;
+        // }
 
+       // Create a new ImageManager instance
+$manager = new ImageManager(new Driver());
+
+// -------------------
+// Handle Brand Icon
+// -------------------
+if ($request->hasFile('brand_icon')) {
+
+    // Delete old icon if exists
+    if ($brand->brand_icon && File::exists(public_path($brand->brand_icon))) {
+        File::delete(public_path($brand->brand_icon));
+    }
+
+    $brandName = Str::slug($request->name);
+    $uniqueId = uniqid();
+    $iconName = "{$brandName}_icon_{$uniqueId}." . $request->file('brand_icon')->getClientOriginalExtension();
+
+    $icon = $manager->read($request->file('brand_icon'));
+    $icon->resize(200, 200, function ($constraint) {
+        $constraint->aspectRatio();
+        $constraint->upsize();
+    });
+
+    $iconPath = 'uploads/brands/icons/';
+    if (!File::exists(public_path($iconPath))) {
+        File::makeDirectory(public_path($iconPath), 0755, true);
+    }
+
+    $icon->save(public_path($iconPath . $iconName));
+    $brand->brand_icon = $iconPath . $iconName;
+} else {
+    if (!isset($brand->brand_icon)) {
+        $brand->brand_icon = null;
+    }
+}
+
+// -------------------
+// Handle Brand Main Image
+// -------------------
+if ($request->hasFile('brand_image')) {
+
+    // Delete old image if exists
+    if ($brand->brand_image && File::exists(public_path($brand->brand_image))) {
+        File::delete(public_path($brand->brand_image));
+    }
+
+    $brandName = Str::slug($request->name);
+    $uniqueId = uniqid();
+    $imageName = "{$brandName}_image_{$uniqueId}." . $request->file('brand_image')->getClientOriginalExtension();
+
+    $image = $manager->read($request->file('brand_image'));
+    $image->resize(800, 800, function ($constraint) {
+        $constraint->aspectRatio();
+        $constraint->upsize();
+    });
+
+    $imagePath = 'uploads/brands/images/';
+    if (!File::exists(public_path($imagePath))) {
+        File::makeDirectory(public_path($imagePath), 0755, true);
+    }
+
+    $image->save(public_path($imagePath . $imageName));
+    $brand->brand_image = $imagePath . $imageName;
+} else {
+    if (!isset($brand->brand_image)) {
+        $brand->brand_image = null;
+    }
+}
         $brand->save();
 
         return redirect()->route('admin.brands.index')->with('success', 'Brand created successfully.');
